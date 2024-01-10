f204f9ce39a82f387911dfe2912050aa981027d0
Chooooooo <suntail2002@naver.com>
2024-01-10 22:20:53 +0900

    Rename: 파일 및 모듈이름 변경
    - 폴더이름에서 -example을 제거

--- cheerio/crawler.js
+++ cheerio/crawler.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const logger = require('../winston/winston');
 
 // 검색 키워드를 사용해서 인프런에서 강의 목록 웹 페이지 가져오기
 const getHtml = async(keyword) => {
     try {
         return await axios.get("https://www.inflearn.com/courses?s=" + encodeURI(keyword));
     } catch (error) {
-        console.error(error);
+        logger.error(error);
     }
 }
 
 // 가져온 웹 페이지에서 강의 제목을 추출해서 배열에 담기
-const parsing = async(keyword) => {
+const parsing = async (keyword) => {
     const html = await getHtml(keyword);
     const $ = cheerio.load(html.data);
     const $courseList = $(".course_card_item");
